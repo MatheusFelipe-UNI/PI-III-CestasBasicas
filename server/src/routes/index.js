dfa434d39089944630e70deec2060b0d5f0d24fd
Natan Lima <99678150+NatanALima@users.noreply.github.com>
2026-03-25 20:21:23 -0300

    feat(server/auth): adiciona lógica de autenticação

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,11 +1,13 @@
 const { Router } = require("express");
-
+const authMiddleware = require("../middlewares/authMiddleware.js");
 //Rotas
+const authRouter = require("./Auth/AuthRouter.js");
 const userRouter = require("./User/UserRouter.js");
 
 const router = Router();
 
-router.use("/users", userRouter);
+router.use("/auth", authRouter);
+router.use("/users", authMiddleware, userRouter);
 router.get("/", (_, res) => res.status(200).json({status: "API FILÉ :)"}))
 
 

d734089a84cd4f4fae1310a763961a5682602477
Natan Lima <99678150+NatanALima@users.noreply.github.com>
2026-05-04 15:06:36 -0300

    feat(client/Input): adiciona input de pesquisa

--- client/src/Components/ActionBar/ActionBar.jsx
+++ client/src/Components/ActionBar/ActionBar.jsx
@@ -1,15 +1,25 @@
 import { FaPlus as IconAdd } from "react-icons/fa";
 
 import styles from "./ActionBar.module.css";
+import { InputSearch } from "../Input/InputSearch/InputSearch";
 
-export function ActionBar({ viewName = "Produto", handleOpenModal}) {
+export function ActionBar({ 
+   viewName = "Produto",
+   searchFilter,
+   updateFilterSearch, 
+   handleOpenModal
+}) {
    return(
       <div className={styles.actionBarContainer}>
          {/* componente InputSearch */}
-         <div style={{display: "flex"}}>
-            <input type="text" />
-            <button style={{backgroundColor: "var(--colorSec)"}}>Filtrar e organizar</button>
-         </div>
+         <InputSearch
+            type="text"
+            name={"searchValue"}
+            id={"searchValue"}
+            placeholder={"Pesquisar..."}
+            value={searchFilter}
+            handleOnChange={updateFilterSearch}
+         />
          <button className={styles.actionBarContent__buttonAdd} onClick={handleOpenModal}>
              <IconAdd/> Cadastrar {viewName}
          </button>
---
+++ client/src/Components/Input/InputSearch/InputSearch.jsx
@@ -0,0 +1,31 @@
+import styles from "./InputSearch.module.css";
+
+export function InputSearch({
+   type = "text",
+   name,
+   id,
+   placeholder,
+   value = "",
+   handleOnChange,
+   handleOnKeyPress,
+   customStyle = {},
+   customClass = ""
+}) {
+   return(
+      <div className={`${styles.inputContainer} ${styles[customClass] || ""}`} style={customStyle}>
+         <div className={styles.inputContent__input}>
+            <input 
+               type={type} 
+               name={name}
+               id={id}
+               placeholder={placeholder}
+               value={value}
+               onKeyUp={handleOnKeyPress}
+               onChange={handleOnChange} 
+               className={styles.input__searchField}
+            />
+         </div>
+         <button className={styles.inputContent__filterButton} title="Filtrar">Filtrar e Organizar</button>
+      </div>
+   )
+}
